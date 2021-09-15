d6b2fa26e6fb4fe774e1d68288910ee70e74e64d
Агарков Тимур <sharpovichjob@gmail.com>
2021-09-15 17:56:34 +0300

    Шаблонизация

--- com/server.go
+++ com/server.go
@@ -1,28 +1,37 @@
 package com
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/index.html")
+	http.ServeFile(w, r, "static/templates/index.html")
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/reg.html")
+	http.ServeFile(w, r, "static/templates/reg.html")
+}
+
+type Data struct {
+	Name string
+	Age  string
 }
 
 func RegComplete(w http.ResponseWriter, r *http.Request) {
-	name := r.FormValue("username")
-	age := r.FormValue("age")
-	fmt.Fprintf(w, "Имя %s, ему %s лет", name, age)
-	// http.ServeFile(w, r, "static/reg_complete.html")
+	data := Data{
+		Name: r.FormValue("username"),
+		Age:  r.FormValue("age"),
+	}
+	tmpl, _ := template.ParseFiles("static/templates/reg_complete.html")
+	tmpl.Execute(w, data)
+
 }
 
 func Routers(port string) {
 	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/auth", GetUser)
 	http.HandleFunc("/reg_complete", RegComplete)
+
 	http.ListenAndServe(port, nil)
 }
