55d1f216e75b797f18e5346126c6ddac4341fcdd
Агарков Тимур <sharpovichjob@gmail.com>
2021-09-21 16:52:45 +0300

    Добавление роутинга в чтение порта

--- com/manage.go
+++ com/manage.go
@@ -7,42 +7,22 @@ import (
 	"project/com/apps"
 )
 
-// func Routers(host, port string) {
-// 	// StaticFile
-// 	fileServer := http.FileServer(http.Dir("./static/"))
-// 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
-// 	// home page
-// 	http.HandleFunc("/", apps.HomePage)
-// 	// auth
-// 	http.HandleFunc("/auth", apps.GetAuth)
-// 	http.HandleFunc("/auth/", apps.GetAuth)
-// 	http.HandleFunc("/auth/postform_authentication", apps.GetUser)
-// 	// review list users
-// 	http.HandleFunc("/users", apps.IndexHandler)
-
-// 	fmt.Printf("Connection web-server on %v%v\n", host, port)
-// 	err := http.ListenAndServe(port, nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
 func Routers(host, port string) {
-	mux := http.NewServeMux()
+	route := http.NewServeMux()
 	// StaticFile
 	fileServer := http.FileServer(http.Dir("./static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	route.Handle("/static/", http.StripPrefix("/static", fileServer))
 	// home page
-	mux.HandleFunc("/", apps.HomePage)
+	route.HandleFunc("/", apps.HomePage)
 	// auth
-	mux.HandleFunc("/auth", apps.GetAuth)
-	mux.HandleFunc("/auth/", apps.GetAuth)
-	mux.HandleFunc("/auth/postform_authentication", apps.GetUser)
+	route.HandleFunc("/auth", apps.GetAuth)
+	route.HandleFunc("/auth/", apps.GetAuth)
+	route.HandleFunc("/auth/postform_authentication", apps.GetUser)
 	// review list users
-	mux.HandleFunc("/users", apps.IndexHandler)
+	route.HandleFunc("/users", apps.IndexHandler)
 
 	fmt.Printf("Connection web-server on %v%v\n", host, port)
-	err := http.ListenAndServe(port, mux)
+	err := http.ListenAndServe(port, route)
 	if err != nil {
 		log.Fatal(err)
 	}
