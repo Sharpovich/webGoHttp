2a1482ee76f69923da24352e95b39d1e752e4417
Агарков Тимур <sharpovichjob@gmail.com>
2021-09-21 16:50:50 +0300

    Добавлена статика

--- com/manage.go
+++ com/manage.go
@@ -7,18 +7,42 @@ import (
 	"project/com/apps"
 )
 
+// func Routers(host, port string) {
+// 	// StaticFile
+// 	fileServer := http.FileServer(http.Dir("./static/"))
+// 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+// 	// home page
+// 	http.HandleFunc("/", apps.HomePage)
+// 	// auth
+// 	http.HandleFunc("/auth", apps.GetAuth)
+// 	http.HandleFunc("/auth/", apps.GetAuth)
+// 	http.HandleFunc("/auth/postform_authentication", apps.GetUser)
+// 	// review list users
+// 	http.HandleFunc("/users", apps.IndexHandler)
+
+// 	fmt.Printf("Connection web-server on %v%v\n", host, port)
+// 	err := http.ListenAndServe(port, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
 func Routers(host, port string) {
+	mux := http.NewServeMux()
+	// StaticFile
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	// home page
-	http.HandleFunc("/", apps.HomePage)
+	mux.HandleFunc("/", apps.HomePage)
 	// auth
-	http.HandleFunc("/auth", apps.GetAuth)
-	http.HandleFunc("/auth/", apps.GetAuth)
-	http.HandleFunc("/auth/postform_authentication", apps.GetUser)
+	mux.HandleFunc("/auth", apps.GetAuth)
+	mux.HandleFunc("/auth/", apps.GetAuth)
+	mux.HandleFunc("/auth/postform_authentication", apps.GetUser)
 	// review list users
-	http.HandleFunc("/users", apps.IndexHandler)
+	mux.HandleFunc("/users", apps.IndexHandler)
 
 	fmt.Printf("Connection web-server on %v%v\n", host, port)
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(port, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
